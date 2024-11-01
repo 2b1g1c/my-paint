d6b55d19a85750b010e2ff8147d93b046c41cee1
Michael Tsukanov <tsukanovWOW@ya.ru>
2024-11-01 19:14:10 +0300

    Add glDebugMessenger

--- src/app/app.hpp
+++ src/app/app.hpp
@@ -10,31 +10,9 @@ namespace mr {
     bool is_glad_inited = false;
 
   public:
-    Application() noexcept {
-      glfwInit();
-      glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-      glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-      glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    Application() noexcept;
 
-#ifdef __APPLE__
-      glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-#endif
-
-      // to make sure no reallocation will be present
-      _windows.reserve(100);
-    }
-
-    Window* create_window(int width, int height, std::string_view handle) {
-      _windows.emplace_back(width, height, handle);
-      if (!is_glad_inited) {
-        is_glad_inited = true;
-        if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
-          std::cout << "Failed to initialize GLAD" << std::endl;
-          exit(30);
-        }
-      }
-      return _windows.data() + _windows.size() - 1;
-    }
+    Window* create_window(int width, int height, std::string_view handle);
 
     ~Application() noexcept {
       glfwTerminate();
