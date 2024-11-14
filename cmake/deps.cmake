34509f7334d914c3ef86d5051a3ea4953e99d8d2
SANEKNAYMCHIK <sashanaumov053@gmail.com>
2024-11-15 00:17:25 +0300

    Remove redundant dependencies from deps.cmake

--- cmake/deps.cmake
+++ cmake/deps.cmake
@@ -6,21 +6,6 @@ file(
 )
 include(${CMAKE_CURRENT_BINARY_DIR}/cmake/CPM.cmake)
 
-if (${CMAKE_HOST_SYSTEM} MATCHES "Linux")
-  CPMAddPackage(
-    NAME nlohmann_json
-    VERSION 3.7.3
-    # the git repo is incredibly large, so we download the archived include directory
-    URL https://github.com/nlohmann/json/releases/download/v3.7.3/include.zip
-    URL_HASH SHA256=87b5884741427220d3a33df1363ae0e8b898099fbc59f1c451113f6732891014
-  )
-
-  if (nlohmann_json_ADDED)
-    add_library(nlohmann_json INTERFACE IMPORTED)
-    target_include_directories(nlohmann_json INTERFACE ${nlohmann_json_SOURCE_DIR}/include)
-  endif()
-endif()
-
 CPMAddPackage(
   NAME imgui_bundle
   GIT_TAG main
@@ -31,11 +16,6 @@ CPMAddPackage(
 set(DEPS_LIBRARIES
   nlohmann_json
 )
-if (WIN32)
-  set(DEPS_LIBRARIES ${DEPS_LIBRARIES} opengl32)
-else()
-  set(DEPS_LIBRARIES ${DEPS_LIBRARIES} GL)
-endif()
 
 set(DEPS_INCLUDE_DIRS
 )
